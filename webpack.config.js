121588185709fd0794340c25897e3ad59ac0d891
Angel Lee <fangyulee0102@gmail.com>
2020-03-29 01:03:11 +0800

    added style

--- webpack.config.js
+++ webpack.config.js
@@ -16,7 +16,11 @@ module.exports = {
                 test: /\.tsx?$/,
                 use: 'ts-loader',
                 exclude: '/node_modules/'
-            }
+            },
+            {
+              test: /\.css$/i,
+              use: ['style-loader', 'css-loader'],
+            },
         ],
     },
     plugins: [

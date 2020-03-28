406c0ac9d01ab4931b4b9df724cd0ade8c496189
Angel Lee <fangyulee0102@gmail.com>
2020-03-28 19:18:20 +0800

    initial commit

---
+++ webpack.config.js
@@ -0,0 +1,31 @@
+const path = require('path');
+// node_modules/.bin/webpack-dev-server --mode development --hot --inline --progress
+module.exports = {
+    mode: 'none',
+    entry: {
+        app: path.join(__dirname, 'src', 'index.tsx')
+    },
+    target: 'web',
+    resolve: {
+      extensions: ['.tsx', '.ts', '.js']
+    },
+    module: {
+        rules: [
+            {
+                test: /\.tsx?$/,
+                use: 'ts-loader',
+                exclude: '/node_modules/'
+            }
+        ],
+    },
+    output: {
+      filename: 'bundle.js',
+      sourceMapFilename: '[file].map',
+      path: path.resolve(__dirname, 'dist'),
+    },
+    // devServer: {
+    //     contentBase: './dist',
+    //     compress: true,
+    //     port: 9000
+    //   }
+}
